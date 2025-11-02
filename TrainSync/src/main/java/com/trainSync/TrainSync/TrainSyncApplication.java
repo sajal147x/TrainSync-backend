8854a33f572bd661129e0eece0d37733c5a4fadc
sajal gupta <sajal147x@gmail.com>
2025-11-02 17:25:57 -0600

    initial spring

---
+++ TrainSync/src/main/java/com/trainSync/TrainSync/TrainSyncApplication.java
@@ -0,0 +1,13 @@
+package com.trainSync.TrainSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainSyncApplication.class, args);
+	}
+
+}
---
+++ TrainSync/src/test/java/com/trainSync/TrainSync/TrainSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainSync.TrainSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
