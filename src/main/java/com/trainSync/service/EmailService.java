0c8f8e089dba4b16db3bf7f2a9a5612ddc0591c6
sajal gupta <sajal147x@gmail.com>
2026-02-03 11:51:53 -0600

    sendgrid

--- src/main/java/com/trainSync/service/EmailService.java
+++ src/main/java/com/trainSync/service/EmailService.java
@@ -1,26 +1,39 @@
 
 package com.trainSync.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import com.trainSync.TrainSyncApplication;
+import com.trainSync.config.exception.GlobalExceptionHandler;
 
 /**
  * Author: Sajal Gupta
  * Created on: Jan 23, 2026 2:16:17 PM
+ * @Comment : using sendgrid api directly instead of javamailsender because java smtp causing issues in production
  */
 @Service
 public class EmailService {
+	 private static final Logger log =
+	            LoggerFactory.getLogger(GlobalExceptionHandler.class);
 	
-	 private final JavaMailSender mailSender;
 	 
-	 public EmailService(JavaMailSender mailSender) {
-		 this.mailSender = mailSender;
-	 }
+	 private final SendGrid sendGrid;
+	 
+	 public EmailService(@Value("${sendgrid.api.key}") String apiKey) {
+	        this.sendGrid = new SendGrid(apiKey);
+	    }
 	
 	/**
 	 * 
@@ -28,15 +41,21 @@ public class EmailService {
 	 */
 	public void sendEmail(String email, String subject, String body) {
 
-       
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("trainsynchronize@gmail.com");
-        message.setTo(email);
-        message.setSubject(subject);
-        message.setText(body);
+		Email from = new Email("trainsynchronize@gmail.com"); // verified sender
+		Email toEmail = new Email(email);
+		Content content = new Content("text/plain", body);
+		Mail mail = new Mail(from, subject, toEmail, content);
 
-        mailSender.send(message);
-    }
+		Request request = new Request();
+		request.setMethod(Method.POST);
+		request.setEndpoint("mail/send");
+		try {
+			request.setBody(mail.build());
+			Response response = sendGrid.api(request);
+		} catch (Exception e) {
+			log.error("SENDGRID EMAIL FAILED", e.getMessage());
+		}
+	}
 
 	
 	public static void main(String[] args) {
